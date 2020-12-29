72c08e87bac6ac06030be16cd43c78b216e02a3f
wstaszewski <wstaszewski@amctech.com>
2020-12-29 09:23:02 +0100

    C# init commit

---
+++ C#/AdventOfCode/Solutions/Year2015/Day01/Solution.cs
@@ -0,0 +1,41 @@
+using System.Linq;
+
+namespace AdventOfCode.Solutions.Year2015
+{
+
+    class Day01 : ASolution
+    {
+        public Day01() : base(1, 2015, "Not Quite Lisp")
+        {
+        }
+
+        protected override string SolvePartOne()
+        {
+            return "232";
+            int countUp = Input.Count(c => c == '(');
+            int countDown = Input.Count(c => c == ')');
+            int res = countUp - countDown;
+            return res.ToString();
+        }
+
+        protected override string SolvePartTwo()
+        {
+            return "1783";
+            int position = 1;
+            int level = 0;
+            foreach (char c in Input.ToCharArray())
+            {
+                if (c == '(')
+                    level++;
+                else if (c == ')')
+                    level--;
+
+                if (level == -1)
+                    break;
+                position++;
+            }
+
+            return position.ToString();
+        }
+    }
+}
